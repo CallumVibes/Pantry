da73d24cad87f00dffce24853b96df73fbbb83f5
CallumCodes <callumjamesfield@proton.me>
2026-07-29 15:34:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  * Bump CACHE_VERSION to force clients onto a new app shell.
  */
 
-const CACHE_VERSION = 'v16';
+const CACHE_VERSION = 'v17';
 const SHELL = `shell-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
